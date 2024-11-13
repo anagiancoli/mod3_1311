bcb6053c71f3a803c3a0ff7bda316cac189d462a
Ana Giancoli <paulagiancoli@gmail.com>
2024-11-13 11:34:47 -0300

    Update first.py

--- first.py
+++ first.py
@@ -1,8 +1,8 @@
 import streamlit as st
 st.write("Hello World")
-respgenero = st.radio('Selecione seu gênero',['Masculino','Feminino'])
-respgen = st.selectbox('Selecione seu gênero',['Masculino','Feminino'])
-respdep = st.multiselect('Escolha um departamento',['DCS', 'DE', 'DIR'])
+respgenero = st.radio('Selecione seu gênero',['Selecione', 'Masculino','Feminino'])
+respgen = st.selectbox('Selecione seu gênero',['Selecione','Masculino','Feminino'])
+respdep = st.multiselect('Escolha um departamento',['Selecione','DCS', 'DE', 'DIR'])
 st.write(respgenero)
 st.write(respgen)
 st.write(respdep)
